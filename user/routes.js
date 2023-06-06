e81d881bd1eb113506047aca0c4318399721db42
nawalirfan <nawal.irfan1110@gmail.com>
2023-06-06 13:57:26 +0500

    cloudinary

--- user/routes.js
+++ user/routes.js
@@ -13,14 +13,14 @@ router.post("/register-login", userController.registerloginUser);
 router.post("/login", userController.login);
 router.post("/forgot", userController.forgotPassword);
 router.post("/forgot/verify", userController.verifyPassword);
-router.get("/getProfile/:id",userController.getByID); //requires authentication
+router.get("/getProfile/:id",userController.getByID);
 router.get("/get-profile", auth.authenticateToken, userController.getProfile);
 router.put("/update_user", userController.updateUser);
 router.patch("/update-user-profile",userController.updateUserProfile)
 router.post("/refresh-token", auth.resetAccessToken);
 //product routes
 router.post("/add-product",upload.array("image"), productController.addProduct); //authentication needs to be added [removed for testing purposes]
-router.patch("/update-product",auth.authenticateToken, productController.updateProduct);
+router.patch("/update-product", productController.updateProduct);
 router.delete("/delete-product", productController.deleteProduct);
 router.get("/get-products", productController.getProducts);
 router.get("/get-products/:id",productController.getProductById);
